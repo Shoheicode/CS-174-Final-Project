db637b6c1a7f74301aa1a7f2e4bc9189946ff2f4
ShoheiCode <ucodejasonirie@gmail.com>
2024-11-21 11:09:43 -0800

    update format

--- main.js
+++ main.js
@@ -834,10 +834,22 @@ function animate() {
 		if(bestTimes.length > 0 && update){
 			let s = ""
 			bestTimes.forEach((value, index)=>{
-				s += "<li>" + value["name"] + "  Time: " + formatTime(elapsedTime) + "</li>"
+				s += "<div class='leaderboard-entry'>";
+				if(index+1 == 1){
+					s += `<span class='rank'>${index+1}st</span>`
+				}
+				else if(index +1 == 2){
+					s += `<span class='rank'>${index+1}nd</span>`
+				}
+				else{
+					s += `<span class='rank'>${index+1}rd</span>`
+				}
+				s += "<span>" + value["name"] + "</span>"
+				s += "<span>" + formatTime(elapsedTime) + "</span>"
+				s += "</div>"
 			})
 
-			document.getElementById("leaderboard").innerHTML += "<ol>"+s + "</ol>"
+			document.getElementById("leaderboard").innerHTML += "<div class='leaderboard-title'>Leaderboard</div> " +s + "</div>"
 			update = false;
 		}
 	}
