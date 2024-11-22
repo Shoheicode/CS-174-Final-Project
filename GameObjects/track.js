86ab2613f98b45fb4a117e3f9a1b7c1bb034979f
ShoheiCode <ucodejasonirie@gmail.com>
2024-11-21 18:55:44 -0800

    update

--- GameObjects/track.js
+++ GameObjects/track.js
@@ -1,5 +1,9 @@
+import * as THREE from 'three'; // Imports the library that we will be using which is the Three.js
+
 class TrackObject{
-    constructor(){
-        
+    constructor(planeGeo, trackMat){
+        this.planeGeo = planeGeo;
+        this.trackMat = trackMat;
+        this.plane = new THREE.Mesh(this.planeGeo, this.trackMat)
     }
 }
