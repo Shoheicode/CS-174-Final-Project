5a5e55f7a1ccb43a00566f5b33a58cb7544baff1
ShoheiCode <ucodejasonirie@gmail.com>
2024-12-07 11:31:03 -0800

    update the comments

--- main.js
+++ main.js
@@ -1689,10 +1689,12 @@ function animate() {
 							cube1.geometry.boundingBox
 						)
 
-						cube2.userData.obb = new OBB()
-						cube2.position.z = obj.position.y+6+2.5
-						cube2.position.y = ((Math.random()-0.5)*2)*5
-						cube2.position.x = ((Math.random()-0.5)*2)*5
+						cube2.userData.obb = new OBB();
+						cube2.position.z = obj.position.y+6+2.5;
+						cube2.position.y = ((Math.random()-0.5)*2)*5;
+						cube2.position.x = ((Math.random()-0.5)*2)*5;
+
+						// Assign properties and material based on the name of the powerup and the character
 						if(obj.name.startsWith("powerup")) {
 							if(carPlayer == "nova"){
 								cube2.material = speedMat;
