cdcbff7e21c417b8e2f5989a546e3d4e52db0cd2
ShoheiCode <ucodejasonirie@gmail.com>
2024-12-18 21:37:41 -0800

    update firebase

--- JSHelperFiles/firebase.js
+++ JSHelperFiles/firebase.js
@@ -9,7 +9,7 @@ import process from 'process';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: `${import.meta.env.VITE_FIREBASE_API_KEY}`,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "finalprojectcs174e.firebaseapp.com",
   projectId: "finalprojectcs174e",
   storageBucket: "finalprojectcs174e.firebasestorage.app",
