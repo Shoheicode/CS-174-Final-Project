66805546ac441a47d0f865d753dc4f57919c949c
ShoheiCode <ucodejasonirie@gmail.com>
2025-03-26 16:31:18 -0700

    update with track object

--- GameObjects/TrackObject.js
+++ GameObjects/TrackObject.js
@@ -1,3 +1,7 @@
 class TrackObject {
-    
+    constructor() {
+        this.trackMaterial = new THREE.MeshBasicMaterial({ color: 0x404040 });
+        this.planeForTrack = new THREE.PlaneGeometry(20, 20)
+        this.plane = new THREE.Mesh(planeForTrack, trackMaterial)
+    }
 }
